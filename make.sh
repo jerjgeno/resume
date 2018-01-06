8dd06bd6b1c7aca311a5dc0b69a63cdff2524305
jerj <johnbrz@gmail.com>
2018-01-05 20:35:58 -0800

    Changed resume.md to master.md

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Creating files..."
-pandoc --from markdown --to plain resume.md > resume.txt
-pandoc --from markdown --to html5 resume.md > resume.html
-pandoc --from markdown --to gfm resume.md > README.md
-pandoc --from markdown --to latex resume.md > resume.tex
+pandoc --from markdown --to plain master.md > resume.txt
+pandoc --from markdown --to html5 master.md > resume.html
+pandoc --from markdown --to gfm master.md > README.md
+pandoc --from markdown --to latex master.md > resume.tex
 echo "All done! ^_^"
