6cc5d2fc7cf9d517d39a1b7ecc13513d560d3a8d
jerj <johnbrz@gmail.com>
2018-01-05 20:49:05 -0800

    Changed output names to jbresume

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Creating files..."
-pandoc --from markdown --to plain master.md > resume.txt
-pandoc --from markdown --to html5 master.md > resume.html
 pandoc --from markdown --to gfm master.md > README.md
-pandoc --from markdown --to latex master.md > resume.tex
+
+pandoc --from markdown --to plain master.md > jbresume.txt
+pandoc --from markdown --to html5 master.md > jbresume.html
+pandoc --from markdown --to latex master.md > jbresume.tex
 echo "All done! ^_^"
