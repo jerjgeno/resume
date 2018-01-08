93bbfc1f8bb42c405a5aa06bfe6247e57d1dfb0f
jerj <johnbrz@gmail.com>
2018-01-07 16:17:09 -0800

    Added Word doc

--- make.sh
+++ make.sh
@@ -5,4 +5,5 @@ pandoc --from markdown --to gfm master.md > README.md
 pandoc --from markdown --to plain master.md > jbresume.txt
 pandoc --from markdown --to html5 master.md > jbresume.html
 pandoc --from markdown --to latex master.md > jbresume.tex
+pandoc --from markdown --to docx master.md -o JohnBrzezickiResume.docx
 echo "All done! ^_^"
